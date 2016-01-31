25bc2b491fd3ca08a59ad6d084a98190a2da99f3
Christopher Chiche <christopher.chiche@gmail.com>
2016-01-31 10:46:40 +0000

    Cleanup code and improve design

--- static/app.js
+++ static/app.js
@@ -1,63 +1,58 @@
 $(function() {
-    $("#search").autocomplete({
-        source: function(request, response) {
-            $.ajax({
-                url: "http://en.wikipedia.org/w/api.php",
-                dataType: "jsonp",
-                data: {
-                    'action': "opensearch",
-                    'format': "json",
-                    'search': request.term
-                },
-                success: function(data) {
-                    response(data[1]);
-                }
-            });
-        }, 
-        select: function(event,ui) {
-            crowl(ui.item.value)
+  $("#search").autocomplete({
+    source: function(request, response) {
+      $.ajax({
+        url: "http://en.wikipedia.org/w/api.php",
+        dataType: "jsonp",
+        data: {
+          'action': "opensearch",
+          'format': "json",
+          'search': request.term
+        },
+        success: function(data) {
+          response(data[1]);
         }
-    });
+      });
+    }, 
+    select: function(event,ui) {
+      crowl(ui.item.value)
+    }
+  });
 });
 
 
-function createStepsList(steps){
-   list = ""
-   $.each(steps,function(i,d){
-      list+="<a href="+d.link+">"+d.title+"</a><br/>"
-   })
-   return list
+function createStepsList(steps) {
+  var list = "";
+  steps.forEach(function(d) {
+    list+="<a href="+d.link+">"+d.title+"</a><br/>";
+  });
+  return list;
 }
 
-function formatTitle(title){
-   return title
+
+function crowl(title) {
+  $("#crowl-steps").html(""); 
+  $("#crowl-result").html("Waiting...");
+  $.ajax({
+    url: "crowl",
+    data: {title}, 
+    success: function(data) {
+      $("#crowl-steps").html(createStepsList(data.steps)); 
+      $("#crowl-result").html(data.result);
+    }, 
+    error: function(data) {
+      $("#crowl-steps").html(""); 
+      $("#crowl-result").html("ERROR");
+    }, 
+    timeout: 30000
+  });
 }
 
-function crowl(title){
-   $("#crowl-steps").html("") 
-   $("#crowl-result").html("Waiting...")
-   $.ajax({
-      url: "crowl",
-      data: {title:formatTitle(title)}, 
-      success: function(data){
-         $("#crowl-steps").html(createStepsList(data.steps)) 
-         $("#crowl-result").html(data.result)
-      }, 
-      error: function(data){
-         $("#crowl-steps").html("") 
-         $("#crowl-result").html("ERROR")
-      }, 
-      timeout: 30000
-   })
+function searchKeyPress(e) {
+  // look for window.event in case event isn't passed in
+  if (typeof e == 'undefined' && window.event) { e = window.event; }
+  if (e.keyCode == 13) {
+    $("#search").autocomplete("close");
+    crowl($('#search').val());
+  }
 }
-
-function searchKeyPress(e){
-// look for window.event in case event isn't passed in
-   if (typeof e == 'undefined' && window.event) { e = window.event; }
-      if (e.keyCode == 13)
-      {
-         value = $('#search').val();
-         $( "#search" ).autocomplete( "close" );
-         crowl(value)
-      }
- }
--- wikipediaCrowl.py
+++ wikipediaCrowl.py
@@ -17,113 +17,108 @@ deprecated=24*60*60 # One day in seconds
 local = os.environ.get('LOCAL') 
 
 def isValid(ref,paragraph):
-   # Check whether the reference is valid in the paragraph
-   if not ref or "#" in ref or "//" in ref or ":" in ref:
-      return False
-   if "/wiki/" not in ref:
-      return False
-   if ref not in paragraph:
-      return False
-   prefix = paragraph.split(ref,1)[0]
-   if prefix.count("(")!=prefix.count(")"):
-      return False
-   return True
+  # Check whether the reference is valid in the paragraph
+  if not ref or "#" in ref or "//" in ref or ":" in ref:
+    return False
+  if "/wiki/" not in ref:
+    return False
+  if ref not in paragraph:
+    return False
+  prefix = paragraph.split(ref,1)[0]
+  if prefix.count("(")!=prefix.count(")"):
+    return False
+  return True
 
 def validateTag(tag):
-   # Check whether the tag is one in which we could find a valid link 
-   name = tag.name
-   isParagraph = name == "p"
-   isList = name == "ul"
-   return isParagraph or isList
+  # Check whether the tag is one in which we could find a valid link 
+  name = tag.name
+  isParagraph = name == "p"
+  isList = name == "ul"
+  return isParagraph or isList
 
 def getSoup(address): 
-   req = urllib2.Request(address, headers={'User-Agent' : "Magic Browser"})
-   data = urllib2.urlopen(req).read()
-   soup = BeautifulSoup(data)
-   soup = soup.find(id="mw-content-text")
-   return soup
+  req = urllib2.Request(address, headers={'User-Agent' : "Magic Browser"})
+  data = urllib2.urlopen(req).read()
+  soup = BeautifulSoup(data)
+  soup = soup.find(id="mw-content-text")
+  return soup
 
 def titleToLink(title): return "/wiki/"+title
 def linkToTitle(link): return link[6:]
 
 def getFirstLink(link):
-   if link in cache:
-      cached = cache[link]
-      if time.time()-cached["time"]<deprecated:
-         return cached["value"]
-   
-   title = linkToTitle(link)
-   print title
-   soup = getSoup("http://en.wikipedia.org/w/index.php?title="+title+"&printable=yes")
-   if not soup: 
-      return False
+  if link in cache:
+    cached = cache[link]
+    if time.time()-cached["time"]<deprecated:
+      return cached["value"]
+  
+  title = linkToTitle(link)
+  soup = getSoup("http://en.wikipedia.org/w/index.php?title="+title+"&printable=yes")
+  if not soup: 
+    return False
 
-   for paragraph in soup.find_all(validateTag, recursive=False):
-      for newLink in paragraph.find_all("a"):
-         ref = newLink.get("href")
-         if isValid(str(ref),str(paragraph)):
-            cache[link]={"value":newLink,"time":time.time()}
-            return newLink
-   return False
+  for paragraph in soup.find_all(validateTag, recursive=False):
+    for newLink in paragraph.find_all("a"):
+      ref = newLink.get("href")
+      if isValid(str(ref),str(paragraph)):
+        cache[link]={"value":newLink,"time":time.time()}
+        return newLink
+  return False
 
 
 def iterateThroughPages(title):
-   steps = []
-   out = []
-   link = "/wiki/"+title
-   result = ""
-   first = True
-   while link is not philosophy_link:
-      if not link:
-         result = "No first link found in: "+steps[-1]
-         break
-      if link == philosophy_link:
-         result = philosophy_title+" found after "+str(len(steps))+" clicks!"
-         break
-      current = getFirstLink(link)
+  steps = []
+  out = []
+  link = "/wiki/"+title
+  result = ""
+  first = True
+  while link is not philosophy_link:
+    if not link:
+      result = "No first link found in: "+steps[-1]
+      break
+    if link == philosophy_link:
+      result = philosophy_title+" found after "+str(len(steps))+" clicks!"
+      break
+    current = getFirstLink(link)
 
-      if not current:
-         result = "No first link in page"
-         break
-      link = current.get("href")
-      title = current.get("title")
-      if link not in steps:
-         steps.append(link)
-         out.append({
-            'link':template+link,
-            'title':title
-         })
-      else:
-         result = "We loop on "+title
-         break
-   return {'result':result, 'steps':out}
+    if not current:
+      result = "No first link in page"
+      break
+    link = current.get("href")
+    title = current.get("title")
+    if link not in steps:
+      steps.append(link)
+      out.append({
+        'link':template+link,
+        'title':title
+      })
+    else:
+      result = "We loop on "+title
+      break
+  return {'result':result, 'steps':out}
 
 @route('/')
 def index():
-   return static_file("index.html", root="/home/ChrisJamesC/wikipediaPhilosophy/static")
-   if local: 
-      return static_file("index.html", root="static")
-   else: 
-      return static_file("index.html", root="/home/ChrisJamesC/wikipediaPhilosophy/static")
+  if local: 
+    return static_file("index.html", root="static")
+  else: 
+    return static_file("index.html", root="/home/ChrisJamesC/wikipediaPhilosophy/static")
 
 @route('/static/<filename>')
 def server_static(filename):
-    return static_file(filename, root='static')
+  return static_file(filename, root='static')
 
 @route('/crowl')
 def crowl():
-   name = request.GET.get('title')
-   title = urllib2.quote(name,safe=":/")
-   if local: 
-      return iterateThroughPages(title)
-   else: 
-      try:
-         return iterateThroughPages(title)
-      except:
-         return {"result": "Internal error", "steps":[]}
+  name = request.GET.get('title')
+  title = urllib2.quote(name,safe=":/")
+  try:
+    return iterateThroughPages(title)
+  except:
+    return {"result": "Internal error", "steps":[]}
 
 if local: 
-   run(host='localhost', port=8080)
+  run(host='localhost', port=8080)
 else: 
-   application = default_app()
+  application = default_app()
 
